82de1859f67b3121e301f5271764dd6770b59041
Danilo Sambugaro <sambugaro.com@gmail.com>
2016-05-01 09:58:50 -0300

    Update bomber.c

--- bomber.c
+++ bomber.c
@@ -87,6 +87,7 @@ void geraM(int minas[M][M]){
 }
 
 void geraRecord(int ponto){
+<<<<<<< HEAD
 
 	FILE *p;
 
@@ -101,6 +102,7 @@ void geraRecord(int ponto){
 	scanf("%c", &nome[5]);
 
 	fprintf(p, "%s %d\n", nome, ponto);
+=======
 	FILE *p;					// Cria ponteiro para arquivo.
 	char nome[5];					// Cria váriavel para armazenar o nome.
 	p=fopen("record.txt", "a+");			// Abre o arquivo record.txt com append (a) e update (+).
@@ -112,6 +114,7 @@ void geraRecord(int ponto){
 	scanf("%s", nome);				// Lê o nome digitado pelo usuário. Necessidade de verificar
 							// se segue o protocolo de 5 caracteres.
 	fprintf(p, "%s %d\n", nome, ponto);		// Escreve no arquivo: NOMEespaçoPONTUAÇÃO
+>>>>>>> 887d5be2c0844f693305ebcc5e2b59bfaa1587c4
 	system("clear");
 	main();
 
